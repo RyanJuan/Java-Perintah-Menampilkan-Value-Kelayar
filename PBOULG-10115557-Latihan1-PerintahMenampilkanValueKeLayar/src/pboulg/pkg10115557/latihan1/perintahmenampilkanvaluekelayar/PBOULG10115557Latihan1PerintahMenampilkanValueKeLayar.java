14b205462ac94465040a47dba14d85261f05c5bd
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-09-25 20:23:26 +0700

    Add files via upload

---
+++ PBOULG-10115557-Latihan1-PerintahMenampilkanValueKeLayar/src/pboulg/pkg10115557/latihan1/perintahmenampilkanvaluekelayar/PBOULG10115557Latihan1PerintahMenampilkanValueKeLayar.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan1.perintahmenampilkanvaluekelayar;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan 1
+ */
+public class PBOULG10115557Latihan1PerintahMenampilkanValueKeLayar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Rizki Adam Kurniawan");
+        System.out.println("ALAMAT\t\t: Jalan Antapani mo 89");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 25 Januari 1991");
+        System.out.println("----------------------");
+        System.out.println("HOBI\t\t: Renang & Lari");
+        System.out.println("MAKES\t:\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+}
